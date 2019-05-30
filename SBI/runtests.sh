7db6d7b7214d04a40308993d45a7b353bcca25a9
Subham Mandal <subhammandal01@gmail.com>
2019-05-30 21:12:33 +0530

    Update runtests.sh

--- SBI/runtests.sh
+++ SBI/runtests.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -x
-pytest --cov=. --cov-report html:coverage
+#pytest --cov=. --cov-report html:coverage
+pytest --cov ./ --cov-report term-missing --cov-report xml --cov-config .coveragerc --junitxml=junit.xml
 
 sleep 3
 
