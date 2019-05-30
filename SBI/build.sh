a1141af02b9b4aeb2b26526e2fc222df8ef1784d
Subham Mandal <subhammandal01@gmail.com>
2019-05-30 16:52:28 +0530

    Update build.sh

--- SBI/build.sh
+++ SBI/build.sh
@@ -6,4 +6,4 @@ export GROUPID=$(id -g)
 echo "Running as UID=$USERID, GID=$GROUPID on branch $BRANCH_NAME"
 cd $(dirname $0)
 [ $BRANCH_NAME == "master" ] && export NEXUS_REPO=nexus-release || export NEXUS_REPO=nexus-snapshot
-docker-compose -f test-bed.yml run --name maven-${BUILD_NUMBER} --rm -w "$WORKSPACE" -e NEXUS_REPO=$NEXUS_REPO --entrypoint "python -m pip install coverage pytest-cov" testpy
+docker-compose -f test-bed.yml run --name maven-${BUILD_NUMBER} --rm -w "$WORKSPACE" -e NEXUS_REPO=$NEXUS_REPO --entrypoint "echo" testpy
