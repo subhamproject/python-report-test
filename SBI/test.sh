7603a993d2c9dc939e14e0595b2d8fee4b628c7e
Subham Mandal <subhammandal01@gmail.com>
2019-05-30 22:38:47 +0530

    Update test.sh

--- SBI/test.sh
+++ SBI/test.sh
@@ -3,5 +3,4 @@ export USERID=$(id -u)
 export PATH="$PATH:/usr/local/bin"
 export GROUPID=$(id -g)
 cd $(dirname $0)
-[ $BRANCH_NAME == "master" ] && export NEXUS_REPO=nexus-release || export NEXUS_REPO=nexus-snapshot
-docker-compose -f test-bed.yml run --rm -w "$WORKSPACE" --name maven-${BUILD_NUMBER} -e NEXUS_REPO=$NEXUS_REPO --entrypoint "SBI/runtests.sh" testpy
+docker-compose -f test-bed.yml run --rm -w "$WORKSPACE" --name maven-${BUILD_NUMBER} --entrypoint "SBI/runtests.sh" testpy
